fa6c2539730b798ea32631a19f44c1c2523063a3
Oxidaner <18622412361@163.com>
2025-04-18 10:56:01 +0800

    项目后端创建

---
+++ oxi-picture-backend/src/main/java/com/zzb/oxipicturebackend/OxiPictureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zzb.oxipicturebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxiPictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OxiPictureBackendApplication.class, args);
+    }
+
+}
---
+++ oxi-picture-backend/src/test/java/com/zzb/oxipicturebackend/OxiPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzb.oxipicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxiPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
